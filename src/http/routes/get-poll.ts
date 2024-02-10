09069f49a0d8b988325838e0f666cb0ce0f1636f
Jader Fellipe <jaderfellip21@gmail.com>
2024-02-09 22:42:32 -0400

    feat: add realtime pub sub to voted polls

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,31 +1,56 @@
-import { FastifyInstance } from 'fastify'
-import { prisma } from '../../lib/prisma'
-import z from 'zod'
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../lib/prisma";
+import z from "zod";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
-  app.get('/polls/:pollId', async (request, reply) => {
+  app.get("/polls/:pollId", async (request, reply) => {
     const getPollParams = z.object({
       pollId: z.string().uuid(),
-     
-    })
-    
-    const { pollId } = getPollParams.parse(request.params)
-    
+    });
+
+    const { pollId } = getPollParams.parse(request.params);
+
     const poll = await prisma.poll.findUnique({
       where: {
-        id: pollId
+        id: pollId,
       },
       include: {
         options: {
           select: {
             id: true,
-            title: true
-          }
-        }
+            title: true,
+          },
+        },
+      },
+    });
+
+    if (!poll) {
+      return reply.status(404).send({ message: "Poll not found" });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+        Object.assign(obj, { [line]: Number(score) });
       }
-    })
-   
-    return reply.send({ poll })
-  })
-  
+      return obj;
+    }, {} as Record<string, number>);
+
+    console.log(votes);
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => ({
+          id: option.id,
+          title: option.title,
+          score: option.id in votes ? votes[option.id] : 0,
+        })),
+      },
+    });
+  });
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,25 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-import { createPoll } from './routes/create-poll'
-import { getPoll } from './routes/get-poll'
-import { voteOnPoll } from './routes/vote-on-poll'
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./routes/ws/poll-results";
 
-const app = fastify()
+const app = fastify();
 app.register(cookie, {
-  secret: 'polls-app-nlw',
-  hook: 'onRequest',
-})
+  secret: "polls-app-nlw",
+  hook: "onRequest",
+});
 
-app.register(createPoll)
-app.register(getPoll)
-app.register(voteOnPoll)
+app.register(websocket);
 
-app.listen({port: 3333}).then(() => {
-  console.log('Server is running on port 3333')
-})
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+
+app.register(pollResults);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
